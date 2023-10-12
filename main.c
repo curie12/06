5c43dddcabbe623e89f39f906bc51a91c768a3cb
Kyuri <curie1211@sm.ac.kr>
2023-10-12 16:08:24 +0900

    02

--- main.c
+++ main.c
@@ -1,38 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sumTwo(int a, int b)
+void func1() //출력값으로 반환할게 없기때문에 void 
 {
-    int add_result=0;
-    add_result = a + b;
-    return add_result; 
+    int x;
+    printf("func1 x is at %p\n", &x); 
 }
-
-int square(int n)
-{
-    return (n*n); 
-}
-
-int get_max(int x, int y)
-{
-    if(x > y)
-        return x;
-    else
-        return y;
-}
+     
 
 int main(int argc, char *argv[])
 {
-    int a, b; //매개변수랑 똑같이 잡을필요 없음. 
-    
-    a = 3;
-    b = 10;
-    printf("sumTwo result : %i\n", sumTwo(a,b));
-    
-    printf("square result : %i\n", square(a));
-    
-    printf("get_max result : %i\n", get_max(a,b));
-    
+    int x;
+    printf("main x is at %p\n", &x);
+    func1();
           
     system("PAUSE");
     return 0;
